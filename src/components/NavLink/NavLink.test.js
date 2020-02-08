cfae9506c6af2c8d5a34578bb75766e55f2576f9
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2020-02-08 13:13:17 +0000

    upgrade deps

--- src/components/NavLink/NavLink.test.js
+++ src/components/NavLink/NavLink.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import NavLink from './NavLink';
 
 describe('NavLink', () => {
-  const wrapper = shallow(<NavLink />);
+  const wrapper = shallow(<NavLink to="" />);
 
   it('renders', () => {
     expect(wrapper).toHaveLength(1);
