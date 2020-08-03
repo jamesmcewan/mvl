77f3696517460b14b0e7b1819ddf5e4cc38ff0e2
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2020-08-03 13:46:01 +0100

    test netlify functions (#27)
    * test netlify functions
    
    * test netlify functions
    
    * test netlify functions
    
    * test netlify functions
    
    * test netlify functions
    
    * test netlify functions
    
    * test netlify functions

--- functions/comics.js
+++ functions/comics.js
@@ -1,6 +1,50 @@
+const axios = require('axios');
+const md5 = require('md5');
+
+const privateKey = process.env.PRIVATE;
+const publicKey = process.env.PUBLIC;
+
+const getData = async (dateDescriptor) => {
+  const source = 'https://gateway.marvel.com/v1/public/comics?';
+  const ts = Date.now().toString();
+  const hash = md5(`${ts}${privateKey}${publicKey}`);
+  const url = `${source}dateDescriptor=${dateDescriptor}&apikey=${publicKey}&ts=${ts}&hash=${hash}`;
+
+  console.log('trying url', url);
+
+  try {
+    const response = await axios.get(`${url}`);
+    const { data } = await response;
+
+    return data;
+  } catch (err) {
+    throw new Error();
+  }
+};
+
 exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: `hello world`
-  };
+  const safePaths = ['thisWeek', 'nextWeek', 'lastWeek'];
+  const { week } = event.queryStringParameters;
+  if (!safePaths.includes(week)) {
+    return {
+      statusCode: 500,
+      body: `you don't have to go home but you can't stay here`
+    };
+  }
+
+  try {
+    const {
+      data: { results }
+    } = await getData(week);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `on no.`
+    };
+  }
 };
