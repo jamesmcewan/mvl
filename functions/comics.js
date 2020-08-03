ed23dc2b24a8ba09f8f540dd7028a0ce5752e8c1
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2020-08-03 14:01:03 +0100

    fix api on netlify

--- functions/comics.js
+++ functions/comics.js
@@ -14,6 +14,7 @@ const getData = async (dateDescriptor) => {
 
   try {
     const response = await axios.get(`${url}`);
+    console.log(response);
     const { data } = await response;
 
     return data;
