785b6957151b21a15239a1c473dff1ab23a976ef
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2020-08-03 12:48:10 +0100

    test netlify functions

--- functions/comics.js
+++ functions/comics.js
@@ -1,6 +1,38 @@
+const axios = require('axios');
+const md5 = require('md5');
+
+const privateKey = process.env.PRIVATE;
+const publicKey = process.env.PUBLIC;
+
+const getData = async (dateDescriptor) => {
+  const source = 'https://gateway.marvel.com/v1/public/comics?';
+  const ts = Date.now().toString();
+  const hash = md5(`${ts}${privateKey}${publicKey}`);
+  const url = `${source}dateDescriptor=${dateDescriptor}&apikey=${publicKey}&ts=${ts}&hash=${hash}`;
+
+  console.log('trying url', url);
+
+  try {
+    const response = await axios.get(`${url}`);
+    const { data } = await response;
+
+    return data;
+  } catch (err) {
+    throw new Error();
+  }
+};
+
 exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: `hello world`
-  };
+  try {
+    const data = await getData('thisWeek');
+    return {
+      statusCode: 200,
+      body: data
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `on no.`
+    };
+  }
 };
