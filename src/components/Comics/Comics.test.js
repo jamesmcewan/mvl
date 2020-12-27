1c6ad17e04aaa948e6bb37080ac938f484c91d13
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2020-12-27 18:58:57 +0000

    tailwind

--- src/components/Comics/Comics.test.js
+++ src/components/Comics/Comics.test.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Comics from './Comics'
-import ComicsWeekTitle from './ComicsWeekTitle'
-import ComicsSection from './ComicsSection'
-import Loading from '../Loading/Loading'
 
 describe('Comics', () => {
   const mockProps = {
@@ -19,15 +16,15 @@ describe('Comics', () => {
     const wrapper = shallow(<Comics {...mockProps} isLoading />)
 
     it('does not include a WeekTitle component', () => {
-      expect(wrapper.find(ComicsWeekTitle)).toHaveLength(0)
+      expect(wrapper.find('h1')).toHaveLength(0)
     })
 
     it('does not include a ComicsSection component', () => {
-      expect(wrapper.find(ComicsSection)).toHaveLength(0)
+      expect(wrapper.find('section')).toHaveLength(0)
     })
 
     it('includes a Loading component', () => {
-      expect(wrapper.find(Loading)).toHaveLength(1)
+      expect(wrapper.find('Loading')).toHaveLength(1)
     })
   })
 
@@ -35,15 +32,15 @@ describe('Comics', () => {
     const wrapper = shallow(<Comics {...mockProps} Loading />)
 
     it('includes a WeekTitle component', () => {
-      expect(wrapper.find(ComicsWeekTitle)).toHaveLength(1)
+      expect(wrapper.find('h1')).toHaveLength(1)
     })
 
     it('includes a ComicsSection component', () => {
-      expect(wrapper.find(ComicsSection)).toHaveLength(1)
+      expect(wrapper.find('section')).toHaveLength(1)
     })
 
     it('does not include a Loading component', () => {
-      expect(wrapper.find(Loading)).toHaveLength(0)
+      expect(wrapper.find('Loading')).toHaveLength(0)
     })
   })
 })
