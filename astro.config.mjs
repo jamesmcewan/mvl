cd3fbd81237bc57b379df9062179eafda7c3cd47
James McEwan <jamesmcewan@safeguardglobal.com>
2022-05-16 16:04:26 +0100

    more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/edge';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({ nodeVersion: '16.x' }),
+  adapter: vercel(),
   integrations: [react(), tailwind()]
 });
