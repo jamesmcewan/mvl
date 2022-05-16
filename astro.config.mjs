a1e57a3aeb35b1b6da31f11942cb9a15e3483eee
James McEwan <jamesmcewan@safeguardglobal.com>
2022-05-16 21:21:05 +0100

    remove axios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/edge';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
