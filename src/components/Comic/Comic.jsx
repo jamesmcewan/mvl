cac8c514357f365ed24807dbc8c7e54c6d0f33cb
James McEwan <jamesmcewan@safeguardglobal.com>
2022-05-16 15:57:10 +0100

    more

--- src/components/Comic/Comic.jsx
+++ src/components/Comic/Comic.jsx
@@ -8,7 +8,12 @@ const getCover = ({ path, extension }) => `${replaceHttp(path)}.${extension}`;
 const Comic = (props) => {
   return (
     <div>
-      <img className="rounded-md" loading="lazy" src={getCover(props.thumbnail)} alt={props.title} />
+      <img
+        className="rounded-md"
+        loading="lazy"
+        src={getCover(props.thumbnail)}
+        alt={props.title}
+      />
       <Details {...props} />
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="bg-gray-700">
-  <div className="mx-auto max-w-7xl p-2 sm:p-6 lg:p-8">
-    <p className='text-white'>Comics are © {new Date().getFullYear()} Marvel</p>
-  </div>
+    <div className="mx-auto max-w-7xl p-2 sm:p-6 lg:p-8">
+      <p className="text-white">
+        Comics are © {new Date().getFullYear()} Marvel
+      </p>
+    </div>
   </footer>
 );
 
