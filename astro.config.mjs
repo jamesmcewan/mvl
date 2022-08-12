a88d7bde9215b14e8133e0d427934ed4ef353244
James McEwan <jamesmcewan@safeguardglobal.com>
2022-08-12 13:45:40 +0100

    upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import react from '@astrojs/react'
 import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: vercel({ nodeVersion: '16.x' }),
   integrations: [react(), tailwind()],
 })
