4966f14aefc3d066ab706177a226ee34bd2f3de4
James McEwan <jamesmcewan@safeguardglobal.com>
2022-10-27 12:07:48 +0100

    using netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-import netlify from '@astrojs/netlify/edge-functions'
+import netlify from '@astrojs/netlify/functions'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
