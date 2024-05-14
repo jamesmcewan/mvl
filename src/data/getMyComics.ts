ea30f2b2540e9c9e6ba8f13776d57256bea41ab2
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2024-05-14 22:04:36 +0100

    update

--- src/data/getMyComics.ts
+++ src/data/getMyComics.ts
@@ -5,11 +5,12 @@ async function getMyComics(week: string) {
   try {
     const response = await fetch(`${url}`)
     const data = await response.json()
+    const { attributionText } = data
     const { results } = data.data
     const regular = results.filter(
       (comic) => comic.variantDescription === '' && comic.format === 'Comic',
     )
-    return regular
+    return { comics: regular, attributionText }
   } catch (e) {
     console.log('oh no', e)
   }
