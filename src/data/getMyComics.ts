b4efdfbe4b6c2a1d71e995345e80d0a9d15b1aff
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2024-07-27 22:53:43 +0100

    debug

--- src/data/getMyComics.ts
+++ src/data/getMyComics.ts
@@ -3,7 +3,9 @@ import getUrl from '../functions/getUrl'
 async function getMyComics(week: string) {
   const url = getUrl(week)
   try {
+    console.log(`get comics from ${URL}`)
     const response = await fetch(`${url}`)
+    console.log(response)
     const data = await response.json()
     const { attributionText } = data
     const { results } = data.data
