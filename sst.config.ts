f0ec060657f30424ff6ce709c7b9699787b64d23
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2024-09-15 00:24:48 +0100

    secrets dont seem to work

--- sst.config.ts
+++ sst.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
+/* tslint:disable */
 /// <reference path="./.sst/platform/config.d.ts" />
-/* eslint-enable */
 
-const MVL_PUBLIC_KEY = new sst.Secret('PUBLICKEY')
-const MVL_MARVEL_KEY = new sst.Secret('MARVELKEY')
+// const MVL_PUBLIC_KEY = new sst.Secret('PUBLICKEY')
+// const MVL_MARVEL_KEY = new sst.Secret('MARVELKEY')
 
 export default $config({
   app(input) {
@@ -16,7 +16,7 @@ export default $config({
   },
   async run() {
     new sst.aws.Astro('mvl-astro', {
-      link: [MVL_PUBLIC_KEY, MVL_MARVEL_KEY],
+      // link: [MVL_PUBLIC_KEY, MVL_MARVEL_KEY],
       domain: {
         name: 'mvl.zom.sh',
         dns: sst.vercel.dns({ domain: 'zom.sh' }),
