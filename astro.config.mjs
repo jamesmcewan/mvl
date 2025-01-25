3d0975ce6915d43910112b81adc9b71961bf52b0
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-01-25 23:21:18 +0000

    update bun.lock for 1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
+  adapter: vercel(),
 })
