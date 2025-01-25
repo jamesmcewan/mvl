61ea7c1c0658e4ad016b3903e67161f2c27589db
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-01-25 23:18:32 +0000

    update bun.lock for 1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
-import netlify from '@astrojs/netlify'
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
-  adapter: netlify(),
 })
