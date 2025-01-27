39d1f2540ce886a95cc87acc8ea1ad3084ca069a
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-01-27 09:22:04 +0000

    tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
   adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
